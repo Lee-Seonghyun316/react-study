46cf5713868a93db4209f188e29c5a21fa6b9ca0
Lee-Seonghyun316 <mn0316@naver.com>
2021-01-17 23:23:11 +0900

    [add] PhoneForm.js PhoneInfoList.js [fix] App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
+import react, { Component} from 'react';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
+
+class App extends Component {
+    id = 2;
+    state = {
+        information: [
+            {
+                id: 0,
+                name: '이성현',
+                phone: '010-0000-0000'
+            },
+            {
+                id: 1,
+                name: '이영준',
+                phone: '010-1111-1111'
+            }
+        ]
+    }
+
+    handleCreate = (date) => {
+        const {information}
+    }
+}
+
+//1
+/*
 import React from 'react';
 import Hello from "./practice/Hello";
 import Wrapper from "./practice/Wrapper";
@@ -11,3 +39,5 @@ function App() {
 }
 
 export default App;
+*/
+ */
---
+++ src/practice/array.js
@@ -0,0 +1,5 @@
+const arr1 = [1, 2, 3, 4, 5];
+
+//3제외
+const arr2=arr1.filter(num=>num!=3);
+console.log(arr2);
